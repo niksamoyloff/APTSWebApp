15fc0d1bd60caed58a87c02dc3c44d1dc40f4b7f
niksamoyloff <niksamoyloff@gmail.com>
2020-01-27 16:43:12 +0300

    Init RepositoryContext factory.

---
+++ DBRepository/Factories/RepositoryContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DBRepository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBRepository.Factories
+{
+    class RepositoryContextFactory : IRepositoryContextFactory
+    {
+        public RepositoryContext CreateDbContext(string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<RepositoryContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new RepositoryContext(optionsBuilder.Options);
+        }
+    }
+}
