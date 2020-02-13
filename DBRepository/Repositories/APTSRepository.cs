9bd085b8366a45b9213c67c53994881cd5dec9b8
niksamoyloff <niksamoyloff@gmail.com>
2020-02-13 09:15:07 +0300

    IAPTSRepository implementation.

--- DBRepository/Repositories/APTSRepository.cs
+++ DBRepository/Repositories/APTSRepository.cs
@@ -112,19 +112,103 @@ namespace DBRepository.Repositories
                 
         }
 
-        public Task DeleteAPTS(object data)
+        public async Task DeleteAPTS(object data)
         {
-            throw new NotImplementedException();
+            var definition = new[] { new { id = "" } };
+            var arrDevDes = JsonConvert.DeserializeAnonymousType(data.ToString(), definition);
+            int devId;
+            List<OicTs> listToDelete = new List<OicTs>();
+
+            using (var context = ContextFactory.CreateDbContext(ConnectionString))
+            {
+                for (int i = 0; i < arrDevDes.Length; i++)
+                {
+                    devId = int.Parse(arrDevDes[i].id);
+                    var tsToDelete = context.OicTs.Find(devId);
+                    if (tsToDelete != null)
+                        listToDelete.Add(tsToDelete);
+                }
+
+                if (listToDelete.Count > 0)
+                {
+                    await AddAction(listToDelete, "Удалил");
+                    context.OicTs.RemoveRange(listToDelete);
+                    await context.SaveChangesAsync();
+                }
+            }
+                
         }
 
-        public Task EditAPTS(object data)
+        public async Task EditAPTS(object data)
         {
-            throw new NotImplementedException();
+            var definition = new { id = "", status = "", comment = "" };
+            var tsDes = JsonConvert.DeserializeAnonymousType(data.ToString(), definition);
+            int tsOicId = Convert.ToInt32(tsDes.id);
+            bool tsStatus = Convert.ToBoolean(tsDes.status);
+            string tsComment = tsDes.comment;
+
+            using (var context = ContextFactory.CreateDbContext(ConnectionString))
+            {
+                var tsList = context.OicTs.Where(item => item.OicId == tsOicId).ToList();
+                if (tsList.Count > 0)
+                {
+                    foreach (var ts in tsList)
+                    {
+                        if (ts != null)
+                        {
+                            ts.IsStatusTs = tsStatus;
+                            ts.Comment = tsComment;
+                        }
+                    }
+                    await context.SaveChangesAsync();
+                }
+            }                
         }
 
-        public Task<JObject[]> ExportDevTreeAPTS()
+        public JObject[] ExportDevTreeAPTS()
         {
-            throw new NotImplementedException();
+            List<JObject> list = new List<JObject>();
+
+            using (var context = ContextFactory.CreateDbContext(ConnectionString))
+            {
+                if (context.OicTs.Count() > 0)
+                {
+                    foreach (var ts in context.OicTs.Where(item => !item.IsRemoved).OrderByDescending(item => item.Id))
+                    {
+                        var device = context.Devices.Where(d => !d.IsRemoved && d.Shifr == ts.DeviceShifr)
+                            .FirstOrDefault();
+                        var eObj = context.PowerObjects.Where(o => !o.IsRemoved && o.Id == context.PowerObjectDevices
+                            .Where(d => device != null && d.DeviceShifr == device.Shifr)
+                            .FirstOrDefault().PowerObjectId)
+                            .FirstOrDefault();
+                        var pSys = context.PowerSystems.Where(s => !s.IsRemoved && eObj != null && s.Id == eObj.PowerSystemId)
+                            .FirstOrDefault();
+                        var primary = context.PrimaryEquipments.Where(p => !p.IsRemoved && p.Shifr == context.PrimaryEquipmentDevices
+                            .Where(p => device != null && p.DeviceShifr == device.Shifr)
+                            .FirstOrDefault().PrimaryEquipmentShifr)
+                            .FirstOrDefault();
+                        if (device != null && eObj != null && pSys != null && primary != null)
+                        {
+                            JObject jObject = JObject.FromObject(new
+                            {
+                                powSys = pSys.Name,
+                                enObj = eObj.Name,
+                                primary = primary.Name,
+                                dev = device.Name,
+                                tsName = ts.Name,
+                                tsId = ts.OicId
+                            });
+                            list.Add(jObject);
+                        }
+                    }
+                }
+            }
+            return list.ToArray();
+        }
+
+        public async Task<JObject[]> ExportDevTreeAPTSAsync()
+        {
+            return await Task.Run(() => ExportDevTreeAPTS());
         }
 
         public Task<JObject[]> GetActions()
