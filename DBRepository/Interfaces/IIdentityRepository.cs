b9f15cf0b2a59621dc5491c6032a208eeb79cad4
niksamoyloff <niksamoyloff@gmail.com>
2020-02-13 09:18:35 +0300

    Add IIdentityRepository.

---
+++ DBRepository/Interfaces/IIdentityRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBRepository.Interfaces
+{
+    public interface IIdentityRepository
+    {
+        string GetUserName();
+        int CheckUserAuthorization();
+    }
+}
