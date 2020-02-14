bf6ac5ad6030cd687a76b5588cca25210fd19e0e
niksamoyloff <niksamoyloff@gmail.com>
2020-02-14 12:23:49 +0300

    Scaffold DbContext.

--- APTSWebApp/Models/APTS_RZA_Context.cs
+++ APTSWebApp/Models/APTS_RZA_Context.cs
@@ -91,9 +91,9 @@ namespace APTSWebApp.Models
 
                 entity.Property(e => e.DeviceShifr).HasMaxLength(128);
 
-                entity.Property(e => e.IsStatusTs).HasColumnName("IsStatusTS");
+                entity.Property(e => e.IsOiсTs).HasColumnName("IsOiсTS");
 
-                entity.Property(e => e.IsOicTs).HasColumnName("IsOicTS");
+                entity.Property(e => e.IsStatusTs).HasColumnName("IsStatusTS");
 
                 entity.HasOne(d => d.DeviceShifrNavigation)
                     .WithMany(p => p.OicTs)
